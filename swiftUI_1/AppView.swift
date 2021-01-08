77242f824ec5c120e798e98095e40e64e17e7e44
thnawat <>
2021-01-08 17:37:53 +0700

    t2

--- swiftUI_1/AppView.swift
+++ swiftUI_1/AppView.swift
@@ -11,7 +11,7 @@ struct AppView: View {
  
     @State private var selectedTab = 0
     @ObservedObject private var order = Order.instance
-     
+    @State var presentingModal = false
     
     var body: some View {
         ZStack(alignment: .topTrailing) {
@@ -50,7 +50,33 @@ struct AppView: View {
         return order.items.count > 0 ? selectedTab != 1 : false
     }
 }
+struct ModalView: View {
+  @Binding var show : Bool
+  var body: some View {
+    ZStack{
+            Rectangle()
+            .fill(Color.white)
+                .opacity(0.5)
 
+            
+                
+            VStack(spacing: 20) {
+                Spacer()
+                Text("Name")
+                Text("Age")
+                Text("Profession")
+                Text("Interest")
+                Spacer()
+            }
+            .frame(minWidth: 300, idealWidth: 300, maxWidth: 300, minHeight: 250, idealHeight: 100, maxHeight: 250, alignment: .top)
+            .background(RoundedRectangle(cornerRadius: 25.0).fill(Color.white))
+            .overlay(RoundedRectangle(cornerRadius: 25.0).stroke(Color.blue, lineWidth: 1.0))
+            
+        }.onTapGesture {
+            self.show = false
+        }
+  }
+}
 struct AppView_Previews: PreviewProvider {
     static var previews: some View {
         AppView()
